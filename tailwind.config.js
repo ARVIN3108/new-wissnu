1bc0788f7443aebac254283442e7a06f86c417d7
ARVIN3108 <70049331+ARVIN3108@users.noreply.github.com>
2024-08-28 15:03:24 +0700

    Cleanup Code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "node_modules/flowbite-react/lib/esm/**/*.js"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
   theme: {
     extend: {
       fontFamily: {
